b8723a1771cf94dee868a0dd2bb920e5634116e8
pujiongling <jiongling.pu@gmail.com>
2023-11-15 09:30:38 +0800

    プロジェクトの作成

---
+++ BlogPujiongling/src/main/java/blog/com/BlogPujionglingApplication.java
@@ -0,0 +1,13 @@
+package blog.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPujionglingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPujionglingApplication.class, args);
+	}
+
+}
